cf98429cac126ee4aff057400047aacc3b5c074f
priyadeepsenwork <work.priyadeepsen@gmail.com>
2025-05-13 23:07:22 +0530

    usermodel made

---
+++ backend/models/user-model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    lastLoginDate: {
+        type: Date,
+        default: Date.now
+    },
+    isVerified: {
+        type: Boolean,
+        default: false 
+    },
+    resetPasswordToken: String,
+    resetPasswordExpiresAt: Date,
+    verificationToken: String,
+    verificationTokenExpiresAt: Date
+}, {timestamps: true})
+
+//timestamps: true ==> createdAt and updatedAt will be
+//added in the schema automatically by mongoose
+
